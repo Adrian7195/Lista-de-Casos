842307b552abb5c39465fba200f35b8c588f80f4
Adrian7195 <143678689+Adrian7195@users.noreply.github.com>
2023-09-15 23:01:37 -0500

    Add files via upload

---
+++ Caso 4.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double soles = 100.0; // Puedes cambiar este valor según la cantidad de soles que desees convertir
+        double tipoCambio = 0.44; // Tipo de cambio de soles a euros
+        double euros = soles * tipoCambio;
+
+        System.out.println("Cantidad en soles: " + soles);
+        System.out.println("Cantidad en euros: " + euros);
+    }
+}
