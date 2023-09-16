8d2df55066c121a20c61ed65bee08b97cd70acde
Adrian7195 <143678689+Adrian7195@users.noreply.github.com>
2023-09-15 22:22:08 -0500

    Add files via upload

---
+++ Caso 2.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declarar y asignar valores a las variables
+        String nombre = "Adrian Flores";
+        String direccion = "Calle Las Moras LT13";
+        String telefono = "980-744-2610";
+
+        // Mostrar los valores de las variables
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Dirección: " + direccion);
+        System.out.println("Teléfono: " + telefono);
+    }
+}
