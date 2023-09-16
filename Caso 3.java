7fe47773c600791b9b71090667a7208b20d420eb
Adrian7195 <143678689+Adrian7195@users.noreply.github.com>
2023-09-15 22:57:57 -0500

    Add files via upload

---
+++ Caso 3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double euros = 100.0; // Puedes cambiar este valor según la cantidad de euros que desees convertir
+        double factorConversion = 166.386; // Factor de conversión de euros a pesetas
+        double pesetas = euros * factorConversion;
+
+        System.out.println("Cantidad en euros: " + euros);
+        System.out.println("Cantidad en pesetas: " + pesetas);
+    }
+}
